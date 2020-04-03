3e2c95180f2591b4f967dd17f9fe6159418dfcc0
Kirill <Lequeston@yandex.ru>
2020-04-03 23:38:08 +0300

    logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,13 @@
 <template lang="pug">
-  
+  .header
+    Logo
 </template>
 
 <script>
+  import Logo from "@/components/Logo";
   export default {
-    name: "Header"
+    name: "Header",
+    components: {Logo}
   }
 </script>
 
@@ -12,4 +15,7 @@
   .header
     width: 100%
     height: 80px
+    display: flex
+    background: gray
+    padding: 0 60px 0 60px
 </style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  .logo
+    img(src="@/assets/logo.png" alt="image")
+    .text {{titleText}}
+</template>
+
+<script>
+  export default {
+    name: "Logo",
+    data(){
+      return{
+        imgLink: "@/assets/logo.png",
+        titleText: "CYCLECOIN",
+      }
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+  .logo
+    display: flex
+    flex-direction: row
+    align-items: center
+    width: 100px
+    height: 100%
+    padding: 20px 0 20px 0
+
+    img
+      height: 100%
+      width: 40px
+      display: block
+      margin: auto
+
+    .text
+      font-size: 15px
+      font-family: Roboto Slab
+      font-style: normal
+      font-weight: normal
+      line-height: 20px
+      display: flex
+      align-items: center
+      text-align: center
+</style>
